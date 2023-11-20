397e79056d047570026df8e5643c5425da2a3084
Fredrik Nääs <frena297@student.liu.se>
2023-11-20 13:02:23 +0100

    up

---
+++ Lab3/bin/dotproduct_precompiled.cpp
@@ -0,0 +1,268 @@
+#define SKEPU_PRECOMPILED
+#define SKEPU_OPENMP
+/**************************
+** TDDD56 Lab 3
+***************************
+** Author:
+** August Ernstsson
+**************************/
+
+#include <iostream>
+
+#include <skepu>
+
+/* SkePU user functions */
+
+/*
+float userfunction(...)
+{
+	// your code here
+}
+
+// more user functions...
+
+*/
+float add(float a, float b){
+	return a + b;
+}
+
+float mult(float a, float b){
+	return a * b;
+}
+
+
+struct skepu_userfunction_skepu_skel_0mult2_mult
+{
+constexpr static size_t totalArity = 2;
+constexpr static size_t outArity = 1;
+constexpr static bool indexed = 0;
+using IndexType = void;
+using ElwiseArgs = std::tuple<float, float>;
+using ContainerArgs = std::tuple<>;
+using UniformArgs = std::tuple<>;
+typedef std::tuple<> ProxyTags;
+constexpr static skepu::AccessMode anyAccessMode[] = {
+};
+
+using Ret = float;
+
+constexpr static bool prefersMatrix = 0;
+
+#define SKEPU_USING_BACKEND_OMP 1
+#undef VARIANT_CPU
+#undef VARIANT_OPENMP
+#undef VARIANT_CUDA
+#define VARIANT_CPU(block)
+#define VARIANT_OPENMP(block) block
+#define VARIANT_CUDA(block)
+static inline SKEPU_ATTRIBUTE_FORCE_INLINE float OMP(float a, float b)
+{
+	return a * b;
+}
+#undef SKEPU_USING_BACKEND_OMP
+
+#define SKEPU_USING_BACKEND_CPU 1
+#undef VARIANT_CPU
+#undef VARIANT_OPENMP
+#undef VARIANT_CUDA
+#define VARIANT_CPU(block) block
+#define VARIANT_OPENMP(block)
+#define VARIANT_CUDA(block) block
+static inline SKEPU_ATTRIBUTE_FORCE_INLINE float CPU(float a, float b)
+{
+	return a * b;
+}
+#undef SKEPU_USING_BACKEND_CPU
+};
+
+
+struct skepu_userfunction_skepu_skel_1add2_add
+{
+constexpr static size_t totalArity = 2;
+constexpr static size_t outArity = 1;
+constexpr static bool indexed = 0;
+using IndexType = void;
+using ElwiseArgs = std::tuple<>;
+using ContainerArgs = std::tuple<>;
+using UniformArgs = std::tuple<float, float>;
+typedef std::tuple<> ProxyTags;
+constexpr static skepu::AccessMode anyAccessMode[] = {
+};
+
+using Ret = float;
+
+constexpr static bool prefersMatrix = 0;
+
+#define SKEPU_USING_BACKEND_OMP 1
+#undef VARIANT_CPU
+#undef VARIANT_OPENMP
+#undef VARIANT_CUDA
+#define VARIANT_CPU(block)
+#define VARIANT_OPENMP(block) block
+#define VARIANT_CUDA(block)
+static inline SKEPU_ATTRIBUTE_FORCE_INLINE float OMP(float a, float b)
+{
+	return a + b;
+}
+#undef SKEPU_USING_BACKEND_OMP
+
+#define SKEPU_USING_BACKEND_CPU 1
+#undef VARIANT_CPU
+#undef VARIANT_OPENMP
+#undef VARIANT_CUDA
+#define VARIANT_CPU(block) block
+#define VARIANT_OPENMP(block)
+#define VARIANT_CUDA(block) block
+static inline SKEPU_ATTRIBUTE_FORCE_INLINE float CPU(float a, float b)
+{
+	return a + b;
+}
+#undef SKEPU_USING_BACKEND_CPU
+};
+
+
+struct skepu_userfunction_skepu_skel_2dotprod_mult
+{
+constexpr static size_t totalArity = 2;
+constexpr static size_t outArity = 1;
+constexpr static bool indexed = 0;
+using IndexType = void;
+using ElwiseArgs = std::tuple<float, float>;
+using ContainerArgs = std::tuple<>;
+using UniformArgs = std::tuple<>;
+typedef std::tuple<> ProxyTags;
+constexpr static skepu::AccessMode anyAccessMode[] = {
+};
+
+using Ret = float;
+
+constexpr static bool prefersMatrix = 0;
+
+#define SKEPU_USING_BACKEND_OMP 1
+#undef VARIANT_CPU
+#undef VARIANT_OPENMP
+#undef VARIANT_CUDA
+#define VARIANT_CPU(block)
+#define VARIANT_OPENMP(block) block
+#define VARIANT_CUDA(block)
+static inline SKEPU_ATTRIBUTE_FORCE_INLINE float OMP(float a, float b)
+{
+	return a * b;
+}
+#undef SKEPU_USING_BACKEND_OMP
+
+#define SKEPU_USING_BACKEND_CPU 1
+#undef VARIANT_CPU
+#undef VARIANT_OPENMP
+#undef VARIANT_CUDA
+#define VARIANT_CPU(block) block
+#define VARIANT_OPENMP(block)
+#define VARIANT_CUDA(block) block
+static inline SKEPU_ATTRIBUTE_FORCE_INLINE float CPU(float a, float b)
+{
+	return a * b;
+}
+#undef SKEPU_USING_BACKEND_CPU
+};
+
+
+struct skepu_userfunction_skepu_skel_2dotprod_add
+{
+constexpr static size_t totalArity = 2;
+constexpr static size_t outArity = 1;
+constexpr static bool indexed = 0;
+using IndexType = void;
+using ElwiseArgs = std::tuple<float, float>;
+using ContainerArgs = std::tuple<>;
+using UniformArgs = std::tuple<>;
+typedef std::tuple<> ProxyTags;
+constexpr static skepu::AccessMode anyAccessMode[] = {
+};
+
+using Ret = float;
+
+constexpr static bool prefersMatrix = 0;
+
+#define SKEPU_USING_BACKEND_OMP 1
+#undef VARIANT_CPU
+#undef VARIANT_OPENMP
+#undef VARIANT_CUDA
+#define VARIANT_CPU(block)
+#define VARIANT_OPENMP(block) block
+#define VARIANT_CUDA(block)
+static inline SKEPU_ATTRIBUTE_FORCE_INLINE float OMP(float a, float b)
+{
+	return a + b;
+}
+#undef SKEPU_USING_BACKEND_OMP
+
+#define SKEPU_USING_BACKEND_CPU 1
+#undef VARIANT_CPU
+#undef VARIANT_OPENMP
+#undef VARIANT_CUDA
+#define VARIANT_CPU(block) block
+#define VARIANT_OPENMP(block)
+#define VARIANT_CUDA(block) block
+static inline SKEPU_ATTRIBUTE_FORCE_INLINE float CPU(float a, float b)
+{
+	return a + b;
+}
+#undef SKEPU_USING_BACKEND_CPU
+};
+
+int main(int argc, const char* argv[])
+{
+	if (argc < 2)
+	{
+		std::cout << "Usage: " << argv[0] << " <input size> <backend>\n";
+		exit(1);
+	}
+	
+	const size_t size = std::stoul(argv[1]);
+	auto spec = skepu::BackendSpec{argv[2]};
+//	spec.setCPUThreads(<integer value>);
+	skepu::setGlobalBackendSpec(spec);
+	
+	
+	/* Skeleton instances */
+	skepu::backend::MapReduce<2, skepu_userfunction_skepu_skel_2dotprod_mult, skepu_userfunction_skepu_skel_2dotprod_add, bool, bool, void> dotprod(false, false);
+	skepu::backend::Reduce1D<skepu_userfunction_skepu_skel_1add2_add, bool, void> add2(false);
+	skepu::backend::Map<2, skepu_userfunction_skepu_skel_0mult2_mult, bool, void> mult2(false);
+//	auto instance = skepu::Map(userfunction);
+// ...
+	
+	/* SkePU containers */
+	skepu::Vector<float> v1(size, 1.0f), v2(size, 2.0f), v3(size, 2.0f);
+
+	v1.randomize(0, 10);
+	v2.randomize(0, 10);
+
+	std :: cout << "v1: " << v1 << "\n";
+	std :: cout << "v2: " << v2 << "\n";
+
+	
+	
+	/* Compute and measure time */
+	float resComb, resSep;
+	
+	auto timeComb = skepu::benchmark::measureExecTime([&]
+	{
+		resComb = dotprod(v1, v2);
+	});
+	
+	auto timeSep = skepu::benchmark::measureExecTime([&]
+	{
+		v3 = mult2(v1, v2);
+		resSep = add2(v3);
+	});
+	
+	std::cout << "Time Combined: " << (timeComb.count() / 10E6) << " seconds.\n";
+	std::cout << "Time Separate: " << ( timeSep.count() / 10E6) << " seconds.\n";
+	
+	
+	std::cout << "Result Combined: " << resComb << "\n";
+	std::cout << "Result Separate: " << resSep  << "\n";
+	
+	return 0;
+}
+
--- Lab3/dotproduct.cpp
+++ Lab3/dotproduct.cpp
@@ -20,7 +20,13 @@ float userfunction(...)
 // more user functions...
 
 */
+float add(float a, float b){
+	return a + b;
+}
 
+float mult(float a, float b){
+	return a * b;
+}
 
 int main(int argc, const char* argv[])
 {
@@ -37,11 +43,21 @@ int main(int argc, const char* argv[])
 	
 	
 	/* Skeleton instances */
+	auto dotprod = skepu::MapReduce <2 >( mult , add );
+	auto add2 = skepu::Reduce(add);
+	auto mult2 = skepu::Map<2>(mult);
 //	auto instance = skepu::Map(userfunction);
 // ...
 	
 	/* SkePU containers */
-	skepu::Vector<float> v1(size, 1.0f), v2(size, 2.0f);
+	skepu::Vector<float> v1(size, 1.0f), v2(size, 2.0f), v3(size, 2.0f);
+
+	v1.randomize(0, 10);
+	v2.randomize(0, 10);
+
+	std :: cout << "v1: " << v1 << "\n";
+	std :: cout << "v2: " << v2 << "\n";
+
 	
 	
 	/* Compute and measure time */
@@ -49,12 +65,13 @@ int main(int argc, const char* argv[])
 	
 	auto timeComb = skepu::benchmark::measureExecTime([&]
 	{
-		// your code here
+		resComb = dotprod(v1, v2);
 	});
 	
 	auto timeSep = skepu::benchmark::measureExecTime([&]
 	{
-		// your code here
+		v3 = mult2(v1, v2);
+		resSep = add2(v3);
 	});
 	
 	std::cout << "Time Combined: " << (timeComb.count() / 10E6) << " seconds.\n";
