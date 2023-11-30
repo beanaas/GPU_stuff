f81b4b59b26575a22923486eaf235196a5c74539
Fredrik Nääs <frena297@student.liu.se>
2023-11-30 08:33:13 +0100

    up

--- Lab3/running_q21.sh
+++ Lab3/running_q21.sh
@@ -2,18 +2,19 @@
 
 rm "output_q21.txt"
 #make clean; make bin/average
-for((i=16;i<=16;i*=2));do
+size=32
+for((i=2;i<=size;i*=2));do
     bin/average data/small.png testing $i CPU  >> output_q21.txt
 done
 echo "done"
-for((i=16;i<=16;i*=2));do
+for((i=2;i<=size;i*=2));do
     bin/average data/small.png testing $i OPENMP  >> output_q21.txt
 done
 echo "done"
-for((i=16;i<=16;i*=2));do
+for((i=2;i<=size;i*=2));do
     bin/average data/small.png testing $i OPENCL  >> output_q21.txt
 done
 echo "done"
-for((i=16;i<=16;i*=2));do
+for((i=2;i<=size;i*=2));do
     bin/average data/small.png testing $i CUDA  >> output_q21.txt 
 done
